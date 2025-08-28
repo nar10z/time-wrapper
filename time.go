386d272c23d2b79e42f774129b63f04f7fd6038e
Nikita Terentyev <terentev_na@magnit.ru>
2025-08-28 14:18:11 +0400

    update

--- time.go
+++ time.go
@@ -1,6 +1,7 @@
 package time_wrapper
 
 import (
+	"bytes"
 	"time"
 
 	"github.com/goccy/go-json"
@@ -41,4 +42,38 @@ func (tw TimeWrapper) MarshalJSON() ([]byte, error) {
 
 	formatted := tw.Format(customLayout)
 	return json.Marshal(formatted)
+}
+
+type DateTimeNoTZ struct{ time.Time }
+
+var jsonNull = []byte("null")
+
+func (t *DateTimeNoTZ) UnmarshalJSON(data []byte) error {
+	if len(data) == 0 || bytes.Equal(data, jsonNull) {
+		*t = DateTimeNoTZ{}
+
+		return nil
+	}
+
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+
+	v, err := time.Parse("2006-01-02T15:04:05", s)
+	if err != nil {
+		return err
+	}
+
+	t.Time = v
+
+	return nil
+}
+
+func (t DateTimeNoTZ) MarshalJSON() ([]byte, error) {
+	if t.Time.IsZero() {
+		return jsonNull, nil
+	}
+
+	return json.Marshal(t.Time.Format("2006-01-02T15:04:05"))
 }
